96042034c6235c216404efacd72e1c827bc2f8d8
Almnf1 <164699611+Almnf1@users.noreply.github.com>
2024-03-24 23:27:07 +0000

    Update project2.py

--- Proj_2/project2.py
+++ Proj_2/project2.py
@@ -520,4 +520,4 @@ def update_table(selected_features, table_shape, forecast_method):
     return table_header, table_info, default_fig, {}
 
 if __name__ == '__main__':
-    app.run(Debug=False)
+    app.run_server(debug=False)
